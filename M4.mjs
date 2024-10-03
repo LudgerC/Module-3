65ef30bcf8b730f079aaaa6293626aefea0c08e0
LudgerC <ludgerfilypecardoso@gmail.com>
2024-10-03 14:45:55 +0200

    Create M4.mjs

---
+++ M4.mjs
@@ -0,0 +1,25 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const userInput = readline.createInterface({ input, output });
+
+
+
+for(let teller = 1; teller <= 100; teller++)
+{
+    if(teller % 3 == 0n&& teller % 5 == 0)
+    {
+        console.log(teller + " FizzBuzz");
+    }
+    else if(teller % 5 == 0)
+    {
+        console.log(teller + " Buzz");
+    }
+    else if(teller % 3 == 0)
+    {
+        console.log(teller + " Fizz");
+    }
+    else 
+    {
+        console.log(teller);
+    }
+}
